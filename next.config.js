f4188cbb60a24c242eac4697285144b056a56be5
Snehil Shah <snehilshah27@gmail.com>
2023-01-30 15:18:31 +0530

    both true

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
       'imagedelivery.net',
     ],
   },
-  assetPrefix : true? 'https://srshah27.github.io/srshah27' : '',
+  assetPrefix : true? 'https://srshah27.github.io/srshah27' : 'https://srshah27.github.io/srshah27',
 }
 
 module.exports = nextConfig
